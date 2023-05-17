f029b97fb297e5e4ec935b3fe08366dd9c6cf446
William <109118303+Ww1ll@users.noreply.github.com>
2023-05-17 17:41:33 -0300

    pagina do carrinho e implementacao do component nav v01

--- assets/src/components/navbar.js
+++ assets/src/components/navbar.js
@@ -1,13 +1,26 @@
-class navbar extends HTMLElement{
+class NavBar extends HTMLElement{
     constructor(){
        super();
        
        const shadow = this.attachShadow({mode: 'open'});
        shadow.appendChild(this.build());
        shadow.appendChild(this.styles());
+       shadow.appendChild(this.script());
     }
 
     build(){
+        const componentRoot = document.createElement('div');
+        componentRoot.setAttribute('class', 'container');
+
+        const toggleBtn = document.createElement('div');
+        toggleBtn.setAttribute('class', 'toggle-btn');
+        toggleBtn.setAttribute('onclick', 'dropdownFunc();');
+
+        // simplificando a utilização do botao spam
+        for(let i = 0; i < 3; i++){
+            const span = document.createElement('span');
+            toggleBtn.appendChild(span);
+        } 
 
 
         return componentRoot;
