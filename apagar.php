50ddcd43348710faae20cbefa2a384e841914dc2
Elgoiabudu <fernandobondeleonelli@gmail.com>
2023-05-17 16:44:29 -0300

    Update apagar.php

--- apagar.php
+++ apagar.php
@@ -4,10 +4,12 @@ Include 'conexao.php';
 
 $id = $_POST["id"];
 
-$sql = mysql_query("delete from cad_user where id = '$id'");
-
-
-
-echo "Feito";
+if ($id == "") {
+    echo "<script>alert("$id")</script>";
+    echo "Sem permissões de acesso!";
+}else{
+    $sql = mysql_query("delete from cad_user where id = '$id'");
+    echo "Feito";
+}
 
 ?>
