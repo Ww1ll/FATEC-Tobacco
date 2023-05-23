8aa7764fa6505af66e0e062e5b1a9f147a5a282d
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-05-23 14:12:58 -0300

    implementação do cad produto

--- cad_prod.php
+++ cad_prod.php
@@ -1,4 +1,7 @@
 <?php
+
+header('Content-Type: text/html; charset=utf-8');
+
 include 'conexao.php';
 
 session_start();
@@ -8,6 +11,18 @@ $img_prod = $_POST['img_prod'];
 $desc_prod = $_POST['desc_prod'];
 $preco_prod = $_POST['preco_prod'];
 
-echo $_SESSION['user_id'];
+$sql = mysql_query("select * from cad_prod where id_prod = '$cod_prod'");
+
+if (mysql_num_rows($sql) > 0) {
+    echo "";
+}else{
+    $sql = mysql_query("insert into cad_prod 
+    (id_prod, img_prod, desc_prod, preco_prod)
+    values
+    ('$cod_prod','$img_prod','$desc_prod','$preco_prod')");
+    
+    echo "Cadastrado com sucesso...";
+}
+
 
 ?>
