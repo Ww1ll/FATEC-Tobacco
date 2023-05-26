b6397203f6d0b58f4578c3730183a573f53b02d6
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-05-26 10:27:37 -0300

    Implementação do carrinho
    70%

--- quem.php
+++ quem.php
@@ -5,7 +5,7 @@ if (isset($_SESSION['usuario_nome'])) {
     $nome = $_SESSION['usuario_nome'];
     $sobrenome = $_SESSION['usuario_sobrenome']; 
     $id = $_SESSION['user_id'];       
-    echo $nome." ".$sobrenome." ".$id;    
+    echo $nome.".";
 }else{
     return false;
 }
